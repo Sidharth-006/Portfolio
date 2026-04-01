6b770508df82e6eeb48faf5638ca4fac4c4f80b6
Sidharth-006 <sidharthsingh3191@gmail.com>
2026-04-01 11:39:14 +0530

    fixing render problem

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/'  // <--- ye important hai
+  base: '/', // ✅ FIXED for Render
 });
